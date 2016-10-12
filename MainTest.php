00460fe3d84d9f6c8e803571bac5c85fe3338dc0
int333 <interesnoe2@gmail.com>
2016-10-13 02:05:31 +0300

    fix approve url check

--- MainTest.php
+++ MainTest.php
@@ -37,7 +37,11 @@ class MainTest extends DRLevTest {
         console("--CHECK EMAIL--\n");
         $script = new DRLevTempMail($this->driver, $this->data);
         $script->start();
-        $this->driver->get($this->data['approve-url']);
+		try {
+			$this->driver->get($this->data['approve-url']);
+		} catch(UnknownServerException $e) {
+			console("ERROR wrong approve url '{$this->data['approve-url']}'");
+		}
 
 //        $this->data['approve-url'] = "https://www.okcupid.com/l/.595de9AxiVga.4Fi9ob21lP2NmPXdlbGNvbWVfZW1haWwAADKTAJMuXZrCV_1CiQAk6g.5CmF8qV9AGymK6GvlSGsI1Ag@@uDM=";
 //        console("--FILL PROFILE--\n");
